871c3d53d64d7512405aaadfea46e139231b5cdb
AbhishekThakur0007 <meabhishekthakur2000@gmail.com>
2024-10-24 19:58:06 +0530

    login page created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { Container, Paper, Typography, TextField, Button } from '@mui/material'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
+    const [isLogin, setLogin] = useState(true)
+    return (
+        <Container maxWidth="sx">
+            <Paper elevation={3}
+                sx={{
+                    padding: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}
+            >
+                {
+                    isLogin ?
+                        <>
+                            <Typography variant="h5">Login</Typography>
+                            <form action="">
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label="Username"
+                                    variant="outlined"
+                                    margin="normal" />
+
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label="Password"
+                                    type='password'
+                                    variant="outlined"
+                                    margin="normal" />
+                                <Button variant='contained' color='primary' type='submit'>
+                                    Login
+                                </Button>
+                            </form>
+                        </>
+                        : <span>register</span>
+                }
+            </Paper>
+        </Container>
+    )
 }
 
 export default Login
